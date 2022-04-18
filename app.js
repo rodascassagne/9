c29566742bd4bdb2c2024809134887bc398864fe
yuyo <rodascassagne@gmail.com>
2022-04-17 19:35:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const x1 = document.querySelector("#mobile-menu");
+const x2 = document.querySelector(".navbar__menu");
+
+x1.addEventListener("click",function(){ x1.classList.toggle("a"); x2.classList.toggle("b");})
